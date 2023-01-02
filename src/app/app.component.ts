16d0f8a0eb580671f2ebc4c39828fd5cd466f1ff
Nikola Badjevic <Ronin95@users.noreply.github.com>
2023-01-02 07:01:02 +0100

    working on the automated color transfer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    this.chooseColorOfPortfolio();
+  }
+  constructor() {}
+
+  randomNum = Math.floor(Math.random()*3)+1;
+  chooseColorOfPortfolio() {
+    console.log(this.randomNum);
+  }
+  // if number === 1
+  // enable everything to be blue
+  // else if number === 2
+  // enable everything to be red
+  // else if number === 3
+  // enable everything to be green
+
 }
