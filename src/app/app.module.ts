5f51b82e6790514373f2408365cb3d695e391dd1
Nikola Badjevic <Ronin95@users.noreply.github.com>
2023-02-05 00:28:51 +0100

    header and about-me finished to 99%

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { IntroTextComponent } from './intro-text/intro-text.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    IntroTextComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/intro-text/intro-text.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-intro-text',
+  templateUrl: './intro-text.component.html',
+  styleUrls: ['./intro-text.component.scss']
+})
+export class IntroTextComponent {
+
+}
