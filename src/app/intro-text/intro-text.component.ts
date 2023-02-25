e6d3a5d183f12e8884146966a390fe024abbc887
Nikola Badjevic <Ronin95@users.noreply.github.com>
2023-02-25 04:14:46 +0100

    working on skills animations in css

--- src/app/intro-text/intro-text.component.ts
+++ src/app/intro-text/intro-text.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-intro-text',
   templateUrl: './intro-text.component.html',
   styleUrls: ['./intro-text.component.scss']
 })
-export class IntroTextComponent {
+export class IntroTextComponent implements OnInit {
+  ngOnInit() {
+      // Get the element
+      let scrollDown = document.getElementById("scroll-down");
 
+      // When clicked, scroll to the bottom of the page
+      scrollDown.addEventListener("click", function() {
+        window.scrollTo({
+          top: document.body.scrollHeight,
+          behavior: "smooth"
+        });
+      });
+  }
 }
--- src/app/skill-set/skill-set.component.ts
+++ src/app/skill-set/skill-set.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skill-set',
   templateUrl: './skill-set.component.html',
   styleUrls: ['./skill-set.component.scss']
 })
-export class SkillSetComponent {
+export class SkillSetComponent implements OnInit {
+  ngOnInit() {}
 
 }
