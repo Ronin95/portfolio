e82563277c328cae8fe00b270ed3df521badf511
Nikola Badjevic <Ronin95@users.noreply.github.com>
2023-02-28 19:17:12 +0100

    now everything is finished how desired - the responsiveness can start

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -6,16 +6,5 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent implements OnInit {
-  ngOnInit() {
-      // Get the element
-      let scroll = document.getElementById("scroll-up");
-
-      // When clicked, scroll to the top of the page
-      scroll.addEventListener("click", function() {
-        window.scrollTo({
-          top: 0,
-          behavior: "smooth"
-        });
-      });
-  }
+  ngOnInit() {}
 }
