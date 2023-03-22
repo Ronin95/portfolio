800cb5a19bd460b0efbcabc2e0fc4492d7b5860b
Nikola Badjevic <Ronin95@users.noreply.github.com>
2023-03-22 18:10:01 +0100

    working on angular contact form - php added

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 
@@ -8,9 +8,13 @@ import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent implements OnInit {
-  @ViewChild('myForm') myForm: any;
+  @ViewChild('myForm') myForm: ElementRef;
+  @ViewChild('nameField') nameField: ElementRef;
+  @ViewChild('emailField') emailField: ElementRef;
+  @ViewChild('messageField') messageField: ElementRef;
+  @ViewChild('sendButton') sendButton: ElementRef;
 
-  contactForm: FormGroup;
+  // contactForm: FormGroup;
 
   ngOnInit() {
     // this.contactForm = new FormGroup({
@@ -20,10 +24,37 @@ export class ContactFormComponent implements OnInit {
     // });
   }
 
-  onSubmit() {
-    console.log(this.contactForm);
-    this.contactForm.reset();
-
+  async onSubmit() {
+    // console.log(this.contactForm);
+    // this.contactForm.reset();
     // action="https://nikola-badjevic.developerakademie.net/send_mail/send_mail.php" method="post"
+    console.log('Sending mail', this.myForm);
+    let nameField = this.nameField.nativeElement;
+    let emailField = this.emailField.nativeElement;
+    let messageField = this.messageField.nativeElement;
+    let sendButton = this.sendButton.nativeElement;
+    nameField.disabled = true;
+    emailField.disabled = true;
+    messageField.disabled = true;
+    sendButton.disabled = true;
+
+    let fd = new FormData();
+    fd.append('name', nameField.value);
+    fd.append('email', emailField.value);
+    fd.append('message', messageField.value);
+
+    let response = await fetch('https://nikola-badjevic.developerakademie.net/send_mail/send_mail.php',
+    {
+      method: 'POST',
+      body: fd
+    });
+
+    let result= await response.text();
+    console.log(result);
+
+    nameField.disabled = false;
+    emailField.disabled = false;
+    messageField.disabled = false;
+    sendButton.disabled = false;
   }
 }
