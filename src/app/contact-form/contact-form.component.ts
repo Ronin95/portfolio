066039e156181e11797a9bdb7a1aa3b1c978d600
Nikola Badjevic <Ronin95@users.noreply.github.com>
2023-04-07 10:08:01 +0200

    working on the backend

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 
@@ -8,7 +8,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent implements OnInit {
-  // @ViewChild('myForm') myForm: ElementRef;
+  @ViewChild('myForm') myForm: NgForm;
   // @ViewChild('nameField') nameField: ElementRef;
   // @ViewChild('emailField') emailField: ElementRef;
   // @ViewChild('messageField') messageField: ElementRef;
@@ -26,37 +26,41 @@ export class ContactFormComponent implements OnInit {
     // this.onSubmit();
   }
 
-  onSubmit(form: NgForm) {
-    console.log(form.value);
-    form.reset();
-    // let nameField = this.nameField.nativeElement;
-    // let emailField = this.emailField.nativeElement;
-    // let messageField = this.messageField.nativeElement;
-    // let sendButton = this.sendButton.nativeElement;
-    // nameField.disabled = true;
-    // emailField.disabled = true;
-    // messageField.disabled = true;
-    // sendButton.disabled = true;
-
-    // let fd = new FormData();
-    // fd.append('name', nameField.value);
-    // fd.append('email', emailField.value);
-    // fd.append('message', messageField.value);
-
-    // let response = await fetch('https://nikola-badjevic.developerakademie.net/send_mail/send_mail.php',
-    // {
-    //   method: 'POST',
-    //   body: fd
-    // });
-
-    // let result= await response.text();
-    // console.log(result);
-
-    // nameField.disabled = false;
-    // emailField.disabled = false;
-    // messageField.disabled = false;
-    // sendButton.disabled = false;
+  onSubmit() {
+    console.log(this.myForm);
   }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form.value);
+  //   form.reset();
+  //   // let nameField = this.nameField.nativeElement;
+  //   // let emailField = this.emailField.nativeElement;
+  //   // let messageField = this.messageField.nativeElement;
+  //   // let sendButton = this.sendButton.nativeElement;
+  //   // nameField.disabled = true;
+  //   // emailField.disabled = true;
+  //   // messageField.disabled = true;
+  //   // sendButton.disabled = true;
+
+  //   // let fd = new FormData();
+  //   // fd.append('name', nameField.value);
+  //   // fd.append('email', emailField.value);
+  //   // fd.append('message', messageField.value);
+
+  //   // let response = await fetch('https://nikola-badjevic.developerakademie.net/send_mail/send_mail.php',
+  //   // {
+  //   //   method: 'POST',
+  //   //   body: fd
+  //   // });
+
+  //   // let result= await response.text();
+  //   // console.log(result);
+
+  //   // nameField.disabled = false;
+  //   // emailField.disabled = false;
+  //   // messageField.disabled = false;
+  //   // sendButton.disabled = false;
+  // }
 
   displayCheckMark() {
     const showImageBtn = document.getElementById('sendButton') as HTMLButtonElement;
@@ -64,9 +68,9 @@ export class ContactFormComponent implements OnInit {
 
     showImageBtn.addEventListener('click', () => {
         imageToDisplay.parentElement?.classList.add('check-marker-visible');
-        // setTimeout(() => {
-        //     imageToDisplay.parentElement?.classList.remove('check-marker-visible');
-        // }, 3000);
+        setTimeout(() => {
+            imageToDisplay.parentElement?.classList.remove('check-marker-visible');
+        }, 3000);
     });
   }
 }
