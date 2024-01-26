ef14b6cc7c3d5db58429fcd30bf50197520a8733
Nikola Badjevic <Ronin95@users.noreply.github.com>
2024-01-26 01:07:06 +0100

    Revert "Revert "Revert "updating styles"""
    This reverts commit 8721254997a9d4e9af7c9e599cb3fd33ef690211.

--- src/app/services/style.service.ts
+++ src/app/services/style.service.ts
@@ -4,40 +4,35 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class StyleService {
-  public randomNum = Math.floor(Math.random() * 3) + 1; // Keep this public so it can be accessed from HeaderMenuComponent
-
-  // Define the image URLs here as well
-  private coverImgUrls = [
-    './assets/img/otherImgs/logo-blueText.png', // Associated with color1 and color2 for randomNum 1
-    './assets/img/otherImgs/logo-redText.png', // Associated with color1 and color2 for randomNum 2
-    './assets/img/otherImgs/logo-greenText.png' // Associated with color1 and color2 for randomNum 3
-  ];
 
   constructor() { }
 
-  public chooseColorOfPortfolio(): void {
-    switch (this.randomNum) {
-      case 1:
-        globalThis.color1 = '#5988FF';
-        globalThis.color2 = '#0043F0';
-        break;
-      case 2:
-        globalThis.color1 = '#FF5959';
-        globalThis.color2 = '#DE0000';
-        break;
-      case 3:
-        globalThis.color1 = '#57E8A2';
-        globalThis.color2 = '#33B476';
-        break;
+  randomNum = Math.floor(Math.random()*3)+1; // A variable that creates a random number between 1-3
+  /**
+   * A function that chooses a random number between 1 and 3.
+   * Based on the number 2 colors are selected.
+   * Those 2 colors are the colors you see in the background and on the website.
+   *
+   * @method
+   * @name chooseColorOfPortfolio
+   * @kind method
+   * @memberof AppComponent
+   * @returns {void}
+   */
+  public chooseColorOfPortfolio() {
+    if (this.randomNum === 1) {
+      globalThis.color1 = '#5988FF';
+      globalThis.color2 = '#0043F0';
+    } else if (this.randomNum === 2) {
+      globalThis.color1 = '#FF5959';
+      globalThis.color2 = '#DE0000';
+    } else if (this.randomNum === 3) {
+      globalThis.color1 = '#57E8A2';
+      globalThis.color2 = '#33B476';
     }
 
+    // the below 2 lines of code set the property to the colors that was chosen in the above condition
     document.body.style.setProperty('--color1', globalThis.color1);
     document.body.style.setProperty('--color2', globalThis.color2);
   }
-
-  // New method to get the selected image URL
-  public getSelectedImageUrl(): string {
-    return this.coverImgUrls[this.randomNum - 1]; // Adjust index to match the randomNum
-  }
 }
-
